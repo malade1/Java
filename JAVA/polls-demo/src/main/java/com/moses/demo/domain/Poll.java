81152e669ce47e1c98124b02657100b191321c94
unknown <malade1@hawk.iit.edu>
2020-11-08 04:07:45 -0600

    added fields and generate getter and setter in poll class

--- JAVA/polls-demo/src/main/java/com/moses/demo/domain/Poll.java
+++ JAVA/polls-demo/src/main/java/com/moses/demo/domain/Poll.java
@@ -1,9 +1,15 @@
 package com.moses.demo.domain;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 
 @Entity
 public class Poll {
@@ -12,6 +18,14 @@ public class Poll {
 	@GeneratedValue
 	@Column(name="poll_id")
 	private Long id;
+	
+	@Column(name="question")
+	private String question;
+
+	@OneToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="poll_id")
+	@OrderBy
+	private Set<Option> options;
 
 	public Long getId() {
 		return id;
@@ -21,10 +35,24 @@ public class Poll {
 		this.id = id;
 	}
 
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public Set<Option> getOptions() {
+		return options;
+	}
+
+	public void setOptions(Set<Option> options) {
+		this.options = options;
+	}
+
 	@Override
 	public String toString() {
-		return "Poll [id=" + id + "]";
-	}
-	
-	
+		return "Poll [id=" + id + ", question=" + question + ", options=" + options + "]";
+	}	
 }
