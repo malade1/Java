2f6dbc0cca1b073f7cb87003a8099ccb463c6a5f
unknown <malade1@hawk.iit.edu>
2020-11-08 04:00:42 -0600

    Generated getter and setter in Option class

--- JAVA/polls-demo/src/main/java/com/moses/demo/domain/Option.java
+++ JAVA/polls-demo/src/main/java/com/moses/demo/domain/Option.java
@@ -15,4 +15,25 @@ public class Option {
 	@Column(name="option_id")
 	private String value;
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Option [id=" + id + ", value=" + value + "]";
+	}
+
 }
