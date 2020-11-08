6c68c42a92dbbc72de58490023d2609e7a97e616
unknown <malade1@hawk.iit.edu>
2020-11-08 02:15:13 -0600

    spring project polls starter

---
+++ JAVA/polls-demo/src/test/java/com/moses/demo/PollsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moses.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
