baa32f10d21c59e2a2d6b8966beb0c3361f1efd2
unknown <malade1@hawk.iit.edu>
2020-11-08 22:19:38 -0600

    added GET and POST method in controller

--- JAVA/polls-demo/src/main/java/com/moses/demo/controller/PollController.java
+++ JAVA/polls-demo/src/main/java/com/moses/demo/controller/PollController.java
@@ -2,9 +2,14 @@ package com.moses.demo.controller;
 
 import javax.inject.Inject;
 
-import org.mockito.InjectMocks;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.moses.demo.domain.Poll;
 import com.moses.demo.repo.PollRepository;
 
 @RestController
@@ -12,4 +17,16 @@ public class PollController {
 
 	@Inject
 	private PollRepository pollRepository;
+	
+	@RequestMapping(value="/polls", method=RequestMethod.GET)
+	public ResponseEntity<Iterable<Poll>> getAllPolls(){
+		Iterable<Poll> allPolls = pollRepository.findAll();
+		return new ResponseEntity<>(pollRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/polls", method=RequestMethod.POST)
+	public ResponseEntity<?> ctreatePolls(@RequestBody Poll poll){
+		poll = pollRepository.save(poll);
+		return new ResponseEntity<>(null, HttpStatus.CREATED);
+	}
 }
